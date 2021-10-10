4042181ad33315c72af43ce1110bd048d57ecdc3
jeremy reiner <reiner_jeremy@yahoo.com>
2021-10-10 13:39:43 +0300

    adding js exercise 1

---
+++ week2/Day1/Exercises/exercise1.js
@@ -0,0 +1,34 @@
+/*Guess the correct outcome to the lines of code written below.
+After guessing the outcome check the results of the code below.
+Check them by copying line by line and running them in the console.*/
+
+let a =5 + "34"; //str + int = 535
+console.log(a);
+let b = 5 - "4"; //str - int = 5 - 4
+console.log(b);
+let c =   10 % 5; // reminder of 10/5 = 2 = even = 0
+console.log(c);
+let d =  5 % 10; // reminder of 5/10 =.5 = not a even number = 1
+console.log(d);
+
+
+let e ="Java" + "Script"; //java Script
+console.log(e);
+let f =    " " + " "; // blank blank
+console.log(f);
+let g=     " " + 0;; // blank 0
+console.log(g);
+let h =    true + true ;//true = 1 + true= 1 = 2
+console.log(h);
+let j    true + false; //true false =1+0 = 1
+console.log(j);
+let k=    false + true; //false=0 true=1
+console.log(k);
+let l =    false - true; //false = 0 - true = 1 = -1
+console.log(l);
+let m =    3 - 4; // -1
+console.log(m);
+let n =    "Bob" - "bill"; //not able to subtract two strings (Nan = not a number)
+console.log(n);
+
+   
