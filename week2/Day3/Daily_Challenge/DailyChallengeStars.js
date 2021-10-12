8296a2c3e2dc8ab65656eb6b2f92fd15087c3dbd
jeremy reiner <reiner_jeremy@yahoo.com>
2021-10-12 21:50:38 +0300

    daily challenge stars

---
+++ week2/Day3/Daily_Challenge/DailyChallengeStars.js
@@ -0,0 +1,37 @@
+// Write a JavaScript program that recreates the pattern below.
+// Do this challenge twice: first by using one loop, then by using two nested for loops (Nested means one inside the other - check out “nested for loops” on Google).
+// Do this Daily Challenge by youself, without looking at the answers on the internet.
+
+
+// *  
+// * *  
+// * * *  
+// * * * *  
+// * * * * *
+// * * * * * *
+
+let star = "*"
+let longStar = "* * * * * *";
+// do
+// {
+//     console.log(star);
+//     star = star + " *";
+// }
+// while (star.length <= longStar.length);
+
+star = "*";
+
+let currentRow = 0
+// outer loop compares star to longStar
+do {
+    currentRow = currentRow + 2;
+
+    // returns another row and adds another star
+    do 
+    {
+        console.log(star)
+        star= star + " *";
+    }
+    while ( star.length <= currentRow);
+    
+} while( star.length <= longStar.length);
