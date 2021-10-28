68a54fd3adadb32539e364d592c6d1f326573048
jeremy reiner <reiner_jeremy@yahoo.com>
2021-10-28 13:14:49 +0300

    exerciseXP updated tic tac toe

---
+++ week4/Day5/Daily_Challenge/daily_challenge.py
@@ -0,0 +1,9 @@
+# Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+# Use List Comprehension
+# Suppose the following input is supplied to the program:
+# without,hello,bag,world
+
+# Then, the output should be:
+# bag,hello,without,world
+# --------------------------------------------------------------------------------
+user_input("Enter a sentence, but instead of spaces, seperate it by commas: ")
--- week4/Day5/Exercises/exerciseXP.py
+++ week4/Day5/Exercises/exerciseXP.py
@@ -17,11 +17,12 @@ def print_board(board):
     print("|" + " " + board['7'] + (" |" + " ") + board['8'] + (" |" + " ")  + board['9'] + " " + "|")
     print("-"*13)
 
-print('''Welcome to tic tac toe. Player 1 will be player X,
-and player 2 will be player O. Please instruct which square you will place your 
-player value. IE first row will be 1, 2, 3 and last row will be 7, 8, 9.
+print('''Welcome to tic tac toe. Player 1 will be player X\n
+and player 2 will be player O. Please doirect where you want to insert your X or O using number values.\n
+IE. First row will be (1, 2, 3) Middle row will be (3, 4, 5) and last row (7, 8, 9).\n
 Good luck!
 ''')
+u_input = input("Would you like to play the game (yes/ no)? ")
 
 def play_game():
     turn = "X"
@@ -39,59 +40,68 @@ def play_game():
             print(f"Your move {move} has already been selected. Chose another spot")
             continue
         if count >= 5:
-            if ttT_board['1'] == ttT_board['2'] == ttT_board['3'] != '':
+            if ttT_board['1'] == ttT_board['2'] == ttT_board['3'] != '': #top line
                 print_board(ttT_board)
                 print("\n Game over..\n")
                 print(f"{turn} won!! Best row to win!")
                 break
-
             
-            elif ttT_board['4'] == ttT_board['5'] == ttT_board['6']  != '':
+            elif ttT_board['4'] == ttT_board['5'] == ttT_board['6']  != '': #middle line
                 print_board(ttT_board)
                 print("\n Game over..\n")
                 print(f"{turn} won!! Well la di da, Great Win!")
                 break
-            
-            elif ttT_board['7'] == ttT_board['8'] == ttT_board['9']  != '':
+
+            elif ttT_board['7'] == ttT_board['8'] == ttT_board['9']  != '': #last line
                 print_board(ttT_board)
                 print("\n Game over..\n")
                 print(f"{turn} won!! Like stealing candy from a baby.")
                 break
             
-            elif ttT_board['1'] == ttT_board['4'] == ttT_board['7']  != '':
+            elif ttT_board['1'] == ttT_board['4'] == ttT_board['7']  != '': #left column
                 print_board(ttT_board)
                 print("\n Game over..\n")
                 print(f"{turn} won!! Could've done better...")
                 break
             
-            elif ttT_board['2'] == ttT_board['5'] == ttT_board['8']  != '':
+            elif ttT_board['2'] == ttT_board['5'] == ttT_board['8']  != '': #middle column
                 print_board(ttT_board)
                 print("\n Game over..\n")
                 print(f"{turn} won!! Horrible way to win...")
                 break
             
-            elif ttT_board['3'] == ttT_board['6'] == ttT_board['9']  != '':
+            elif ttT_board['3'] == ttT_board['6'] == ttT_board['9']  != '': #right column
                 print_board(ttT_board)
                 print(f"\n Game over..\n {turn} won!! Took you long enough...")
                 break
             
-            elif ttT_board['1'] == ttT_board['4'] == ttT_board['9']  != '':
+            elif ttT_board['1'] == ttT_board['5'] == ttT_board['9']  != '': # digonal right
                 print_board(ttT_board)
                 print("\n Game over..\n")
                 print(f"{turn} won!! Bloody hell harry...")
                 break
+
+            elif ttT_board['3'] == ttT_board['5'] == ttT_board['7']  != '': # digonal left
+                print_board(ttT_board)
+                print("\n Game over..\n")
+                print(f"{turn} won!! Bloody hell harry...")
+                break
+
             elif count == 9:
                 print_board(ttT_board)
                 print("\nGAME OVER\n" + "Welp.. I guess nobody lost")
                 break
-
-
 
         if turn == "X":
             turn = "O"
         else:
             turn = "X"
 
-play_game()
+def play():
+    if u_input == "yes":
+        play_game()
+    else:
+        print("Have a good day!")
+play()
 
 
