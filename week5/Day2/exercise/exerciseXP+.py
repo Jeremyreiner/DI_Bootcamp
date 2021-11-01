493d7689fbbf8f0880d2f968eeb38717eed9b8c9
jeremy reiner <reiner_jeremy@yahoo.com>
2021-11-02 00:47:10 +0200

    updates to exercises

--- week5/Day2/exercise/exerciseXP+.py
+++ week5/Day2/exercise/exerciseXP+.py
@@ -5,17 +5,43 @@
 # last_name : (string)
 # Initial members data:
 class Family():
-    def __init__(self, *members, last_name):
-        self.members = members
+    def __init__(self, last_name, members = None):
         self.last_name = last_name
+        if members is None:
+            self.members = []
+        else:
+            self.members = members
+
+    def add_members(self, member):
+        if member not in self.members:
+            self.members.append(member)
+    
+
+    def print_family(self):
+        for member in self.members:
+            print(f"Wow {member}. What a family you have!")
+
+
+class Members(Family):
+    def __init__(self, name, age, gender, is_child = False):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.is_child = is_child
+        # member_info = {f"'name':{self.name}, 'age': {self.age}, 'gender': {self.gender}, 'is_child' : {is_child}"}
+        # self.members.append(member_info)
+
+
 
 # Implement the following methods:
 
 # born: adds a child to the members list (use **kwargs), don’t forget to print a message congratulating the family.
 # is_18: takes the name of a family member as a parameter and returns True if they are over 18 and False if not.
 # a method that prints all the members of the family.
-    def born(self):
-        self.members.append({'name':'jimmy','age':0,'gender':'Female','is_child':True})
 
-my_family = Family({'name':'Michael','age':35,'gender':'Male','is_child':False}, "reiner")
-print(my_family.members)
+member1 = Members("Jeremy", 27, "Male", is_child= False)
+member2 = Members("Stasia", 20, "Female", is_child= False)
+family1 = Family("Reiner")
+family1.add_members(member1.name)
+family1.add_members(member2.name)
+family1.print_family()
--- week5/Day2/exercise/module.py
+++ week5/Day2/exercise/module.py
@@ -1,33 +1,45 @@
-#  Create a class called Dog with the following attributes name, age, weight.
-class Dog():
-    def __init__(self, name, age, weight):
+# Exercise 3 : Dogs Domesticated
+# Instructions
+# ----------------------------
+from exerciseXP import Dog
+import random
+# create a class named PetDog 
+# that inherits from Dog.
+class PetDog(Dog):
+    # Add an attribute called trained to the __init__ method,
+    #  this attribute is a boolean and the value should be False
+    #  by default.
+
+    def __init__(self, name,age, weight, trained = False):
+        super().__init__(name, age, weight)
         self.name = name
-        self.age = age
-        self.weight = weight
-# Implement the following methods in the Dog class:
-# bark: returns a string which states: “<dog_name> is barking”.
-    def bark(self):
-        print(f"{self.name} is barking. BOW WOW")
+        self.trained = trained
     
-# run_speed: returns the dogs running speed (weight/age*10).
-    def run_speed(self):
-        return (self.weight/ self.age) * 10
+    # train: prints the output of bark and switches the 
+    # trained boolean to True
+    def trained(self, trained = True):
+            return Dog.bark()
 
+    # play: takes a parameter which value is a few names of others
+    # (use *args). The method should print the following string:
+    #  “dog_names all play together”.
+    def play(self, *args):
+        
+        print(f"{self.name} are all playing together" )
     
-# fight : takes a parameter which value should be another dog called other_dog, returns a string stating which dog won the fight. 
-# The winner should be the dog with the higher run_speed x weight.
-    def fight(self, other_dog):
-        if self.run_speed() > other_dog.run_speed():
-            print(f'{self.name} has won the fight')
-        else:
-            print(f'{other_dog} has won the fight')
+    # do_a_trick: If the dog is trained the method should print
+    #  one of the following sentences at random:
+    # “dog_name does a barrel roll”.
+    # “dog_name stands on his back legs”.
+    # “dog_name shakes your hand”.
+    # “dog_name plays dead”.
+    def do_a_trick(self):
+        if self.trained == True:
+            tricks = [f'{self.name} does a barrel roll', f'{self.name} stands on his back legs', f'{self.name} shakes your hand', f'{self.name} plays dead']
+            print(random.choices(tricks))
 
 
-# Create 3 dogs and run them through your class.
-dog1 = Dog("jet", 5, 25)
-dog2 = Dog("shasta", 8, 20)
-dog3 = Dog("jj", 4, 35)
-print(dog1.run_speed())
-print(dog2.run_speed())
-print(dog3.run_speed())
-dog3.fight(dog1) 
+new_dog1 = PetDog("sheryll", 2, 10)
+new_dog = PetDog("tom", 5, 20)
+new_dog1.play()
+
