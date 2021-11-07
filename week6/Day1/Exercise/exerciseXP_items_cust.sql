76066b8dc14df48f33fc64235e28b44e852b3fe8
jeremy reiner <reiner_jeremy@yahoo.com>
2021-11-07 14:03:59 +0200

    sql exercise day 1 files

---
+++ week6/Day1/Exercise/exerciseXP_items_cust.sql
@@ -0,0 +1,6 @@
+-- select * from items;
+-- select * from items where price > 80
+-- select * from items where price <= 300
+-- select * from customers where last_name  = 'smith'
+-- select * from customers where last_name = 'jones'
+select * from customers where first_name != 'scott'
